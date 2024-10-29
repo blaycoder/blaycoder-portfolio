873561195ee605d8c89febcb284161bed7c94259
blaycoder <onatola4real@gmail.com>
2024-10-29 22:09:59 +0100

    made changes to link

--- src/components/ProjectContainer/ProjectContainer.jsx
+++ src/components/ProjectContainer/ProjectContainer.jsx
@@ -1,23 +1,24 @@
-import uniqid from 'uniqid'
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LaunchIcon from '@material-ui/icons/Launch'
-import './ProjectContainer.css'
+import uniqid from "uniqid";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LaunchIcon from "@material-ui/icons/Launch";
+import "./ProjectContainer.css";
+import PropTypes from 'prop-types';
 
 const ProjectContainer = ({ project }) => (
-  <div className='project'>
+  <div className="project">
     <h3>{project.name}</h3>
 
-    <p className='project__description'>{project.description}</p>
+    <p className="project__description">{project.description}</p>
     {project.credit && (
-      <p className='project__description'>
+      <p className="project__description">
         <strong>Credit:</strong> {project.credit}
       </p>
     )}
 
     {project.stack && (
-      <ul className='project__stack'>
+      <ul className="project__stack">
         {project.stack.map((item) => (
-          <li key={uniqid()} className='project__stack-item'>
+          <li key={uniqid()} className="project__stack-item">
             {item}
           </li>
         ))}
@@ -27,8 +28,8 @@ const ProjectContainer = ({ project }) => (
     {project.sourceCode && (
       <a
         href={project.sourceCode}
-        aria-label='source code'
-        className='link link--icon'
+        aria-label="source code"
+        className="link link--icon"
       >
         <GitHubIcon />
       </a>
@@ -37,15 +38,25 @@ const ProjectContainer = ({ project }) => (
     {project.livePreview && (
       <a
         href={project.livePreview}
-        aria-label='live preview'
-        className='link link--icon'
-        target='_blank'
-        noreferrer
+        aria-label="live preview"
+        className="link link--icon"
+        target="_blank"
+        rel="noreferrer"
       >
         <LaunchIcon />
       </a>
     )}
   </div>
-)
+);
+ProjectContainer.propTypes = {
+  project: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    credit: PropTypes.string,
+    stack: PropTypes.arrayOf(PropTypes.string),
+    sourceCode: PropTypes.string,
+    livePreview: PropTypes.string,
+  }).isRequired,
+};
 
-export default ProjectContainer
+export default ProjectContainer;
