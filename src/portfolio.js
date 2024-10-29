9a29d593b6ffa97f22e80d78a6c6c51654ff942a
blaycoder <onatola4real@gmail.com>
2024-10-29 22:55:10 +0100

    added new projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
-  homepage: 'https://github.com/blaycoder',
-  title: 'OA',
-}
+  homepage: "https://github.com/blaycoder",
+  title: "OA",
+};
 
 const about = {
   name: "Ayomide",
@@ -22,80 +22,112 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'An Online Bookstore',
+    name: "An Online Bookstore",
     description:
-      'Developed an e-commerce platform for selling books online, designed to provide a user-friendly experience for both customers and administrators.',
+      "Developed an e-commerce platform for selling books online, designed to provide a user-friendly experience for both customers and administrators.",
     credit:
-      'This project was created during my tenure at AKI Solutions. Copyright AKI Solutions.',
-    stack: ['WordPress'],
-    livePreview: 'https://simcomfort.co.uk/',
+      "This project was created during my tenure at AKI Solutions. Copyright AKI Solutions.",
+    stack: ["WordPress"],
+    livePreview: "https://simcomfort.co.uk/",
   },
   {
-    name: 'TBS Academy Website',
+    name: "TBS Academy Website",
     description:
-      ' The website offers a range of educational resources and courses designed to enhance the learning experience for students. ',
+      " The website offers a range of educational resources and courses designed to enhance the learning experience for students. ",
     credit:
-      'This project was created during my tenure at AKI Solutions. Copyright AKI Solutions.',
-    stack: ['WordPress'],
-    livePreview: 'https://mytbsacademy.com/',
+      "This project was created during my tenure at AKI Solutions. Copyright AKI Solutions.",
+    stack: ["WordPress"],
+    livePreview: "https://mytbsacademy.com/",
   },
   {
     name: "Simi's Birthday",
-    description: 'Developed a personalized birthday webpage',
-    stack: ['TailwindCSS', 'ReactJs'],
-    sourceCode: 'https://github.com/blaycoder/simi-birthday',
-    livePreview: 'https://simi-birthday.vercel.app/',
+    description: "Developed a personalized birthday webpage",
+    stack: ["TailwindCSS", "ReactJs"],
+    sourceCode: "https://github.com/blaycoder/simi-birthday",
+    livePreview: "https://simi-birthday.vercel.app/",
   },
   {
-    name: 'GitHub Repo Portfolio',
+    name: "GitHub Repo Portfolio",
     description:
-      'Developed a portfolio website designed to showcase GitHub repositories. The application pulls data from GitHub to display project details, such as descriptions, languages used, and stars, in an organized and visually appealing format.',
-    stack: ['TailwindCSS', 'ReactJs', 'Vercel', 'GitHub API'],
-    sourceCode: 'https://github.com/blaycoder/github-repo-portfolio',
-    livePreview: 'https://github-repo-portfolio.vercel.app/',
+      "Developed a portfolio website designed to showcase GitHub repositories. The application pulls data from GitHub to display project details, such as descriptions, languages used, and stars, in an organized and visually appealing format.",
+    stack: ["TailwindCSS", "ReactJs", "Vercel", "GitHub API"],
+    sourceCode: "https://github.com/blaycoder/github-repo-portfolio",
+    livePreview: "https://github-repo-portfolio.vercel.app/",
   },
   {
-    name: 'Coloured Text',
+    name: "Carefinder",
     description:
-      'Developed a web application for generating colored text. The tool allows users to input text and apply different colors, offering a simple and intuitive interface for creating visually engaging content. ',
-    stack: ['HTML', 'CSS', 'Javascript', 'GitHub Pages'],
-    sourceCode: 'https://github.com/blaycoder/Coloured-Text',
-    livePreview: 'https://blaycoder.github.io/Coloured-Text/',
+      "Developed a web application to help find the nearest hospital or clinic in Nigeria. The application provides users with a list of healthcare facilities based on their location, allowing them to easily access medical services.",
+    stack: ["TailwindCSS", "ReactJs", "Vercel", "NextJs"],
+    sourceCode: "https://github.com/blaycoder/carefinder-project",
+    livePreview: "https://carefinder-six.vercel.app/",
   },
   {
-    name: 'Simple Pizza Machine',
+    name: "Coffee Shop Website",
     description:
-      'Created a simple web application that simulates a pizza ordering process. ',
-    stack: ['HTML', 'CSS', 'Javascript', 'GitHub Pages'],
-    sourceCode: 'https://github.com/blaycoder/simple-pizza-machine',
-    livePreview: 'https://blaycoder.github.io/simple-pizza-machine/',
+      "Developed a website for a coffee shop, providing customers with information about the menu, location, and contact details. The site features a responsive design and interactive elements to enhance the user experience.",
+    stack: ["HTML", "CSS", "Javascript", "GitHub Pages"],
+    sourceCode: "https://github.com/blaycoder/our-coffee-shop",
+    livePreview: "https://blaycoder.github.io/our-coffee-shop/",
   },
-]
+  {
+    name: "School Management System Dashboard",
+    description:
+      "Developed a dashboard for a school management system with a team member Boluwatife Oluwagbamila (https://github.com/Boluwatt) as a project, designed to provide administrators with an overview of student data, attendance, and performance. The dashboard features interactive charts and tables for easy data visualization.",
+    stack: ["HTML", "CSS", "Javascript", "GitHub Pages"],
+    sourceCode: "https://github.com/blaycoder/Eschoolpedia-student-dashboard",
+    livePreview: "https://blaycoder.github.io/Eschoolpedia-student-dashboard",
+  },
+  {
+    name: "Coloured Text",
+    description:
+      "Developed a web application for generating colored text. The tool allows users to input text and apply different colors, offering a simple and intuitive interface for creating visually engaging content. ",
+    stack: ["HTML", "CSS", "Javascript", "GitHub Pages"],
+    sourceCode: "https://github.com/blaycoder/Coloured-Text",
+    livePreview: "https://blaycoder.github.io/Coloured-Text/",
+  },
+  {
+    name: "Phone Number Validator",
+    description:
+      "Developed a web application for validating phone numbers. The tool checks if a phone number is valid based on the country code and number format, providing users with instant feedback on the input.",
+    stack: ["HTML", "CSS", "Javascript", "GitHub Pages", "NumVerify API"],
+    sourceCode: "https://github.com/blaycoder/phone-number-validator",
+    livePreview: "https://blaycoder.github.io/phone-number-validator/",
+  },
+  {
+    name: "Simple Pizza Machine",
+    description:
+      "Created a simple web application that simulates a pizza ordering process. ",
+    stack: ["HTML", "CSS", "Javascript", "GitHub Pages"],
+    sourceCode: "https://github.com/blaycoder/simple-pizza-machine",
+    livePreview: "https://blaycoder.github.io/simple-pizza-machine/",
+  },
+];
 
 const projectsLink = {
-  link: 'https://github.com/blaycoder',
-}
+  link: "https://github.com/blaycoder",
+};
 
 const skills = [
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'ReactJs',
-  'VueJs',
-  'WordPress',
-  'Shopify',
-  'Material UI',
-  'Git',
-  'GitHub Pages',
-  'Framer Motion',
-  'Figma',
-  'Adobe Photoshop',
-  'CorelDraw',
-]
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "ReactJs",
+  "VueJs",
+  "WordPress",
+  "Shopify",
+  "Material UI",
+  "Git",
+  "GitHub Pages",
+  "Framer Motion",
+  "Figma",
+  "Adobe Photoshop",
+  "CorelDraw",
+];
 
 const contact = {
-  email: 'ayomideonatola@gmail.com',
-}
+  email: "ayomideonatola@gmail.com",
+};
 
-export { header, about, projects, projectsLink, skills, contact }
+export { header, about, projects, projectsLink, skills, contact };
