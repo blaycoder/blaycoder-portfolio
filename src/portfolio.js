0201a6ef01c6e1f9d81b46bb7b1caac07af02242
Ayomide-AKI <onatola.ayomide@eschool.ng>
2025-07-06 17:49:49 +0100

    updated portfolio:

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,7 @@ const about = {
   description:
     "I'm a Frontend Developer and an alumnus of Altschool Africa. I'm passionate about sharing knowledge, documentation, contributing to open source and others. I’m currently learning at Alx Africa. I create technical content (on my Medium, and YouTube). I build useful projects. I believe the impossible can become possible if a step is taken and focus is constant.",
   resume:
-    "https://drive.google.com/file/d/1A0LYqqSDUD3dglzGq4CcX3YOLaYL2sZE/view?usp=sharing",
+    "https://drive.google.com/file/d/1yi9G4Imf1Ie41fKmY41OUVvJvWSG0wfM/view?usp=sharing",
   social: {
     linkedin: "https://www.linkedin.com/in/ayomide-onatola-3180281a5/",
     github: "https://github.com/blaycoder",
